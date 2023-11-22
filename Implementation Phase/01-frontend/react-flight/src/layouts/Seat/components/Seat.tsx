86a1435a27682861fe9c5a009732f4efd66e7bd7
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-22 08:21:33 -0700

    cleanup

--- Implementation Phase/01-frontend/react-flight/src/layouts/Seat/components/Seat.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Seat/components/Seat.tsx
@@ -2,17 +2,10 @@ import SeatModel from "../../../models/SeatModel";
 
 export const Seat: React.FC<{ seat: SeatModel }> = (props) => {
     return (
-        // <a className={'list-group-item list-group-item-check d-flex gap-2'} href={'#'} aria-current='true'>
-        //     <div className={'input-group'}>
-        //         <div className={'input-group-text'}>
-        //             {props.seat.code}
-        //         </div>
-        //     </div>
-        // </a>
         <>
-            <input type={'radio'} className={'btn-check'} name={'seatOptions'} id={props.seat.code}
-                   autoComplete={'off'}/>
-            <label className={'btn btn-secondary'} htmlFor={props.seat.code}>{props.seat.code}</label>
+            <input type='radio' className='btn-check m-1' name='seatOptions' id={props.seat.code}
+                   autoComplete='off' disabled={!props.seat.available}/>
+            <label className='btn btn-primary' htmlFor={props.seat.code}>{props.seat.code}</label>
         </>
     );
 }
