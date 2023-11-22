9e92f39e877fec88c8ed341c453f37e3f69a7eaf
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-22 09:00:39 -0700

    corrected input type

--- Implementation Phase/01-frontend/react-flight/src/layouts/Login/LoginPage.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Login/LoginPage.tsx
@@ -3,7 +3,7 @@ export const LoginPage = () => {
         <form>
             <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
             <div className='form-floating'>
-                <input id='usernameInput' className='form-control' type='username' placeholder='Username'/>
+                <input id='usernameInput' className='form-control' type='text' placeholder='Username'/>
                 <label htmlFor='usernameInput'>Username</label>
             </div>
             <div className='form-floating'>
