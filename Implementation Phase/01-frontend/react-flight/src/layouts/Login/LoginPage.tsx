f2ee2e66515bd129c1da7cbdd890a7beb5989faf
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-22 09:47:32 -0700

    aligned content to center

--- Implementation Phase/01-frontend/react-flight/src/layouts/Login/LoginPage.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Login/LoginPage.tsx
@@ -1,16 +1,18 @@
 export const LoginPage = () => {
     return (
-        <form>
-            <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
-            <div className='form-floating'>
-                <input id='usernameInput' className='form-control' type='text' placeholder='Username'/>
-                <label htmlFor='usernameInput'>Username</label>
-            </div>
-            <div className='form-floating'>
-                <input id='passwordInput' className='form-control' type='password' placeholder='Password'/>
-                <label htmlFor='passwordInput'>Password</label>
-            </div>
-            <button className='btn btn-primary w-100 py-2' type='submit'>Sign in</button>
-        </form>
+        <div className='form-signin w-100 m-auto'>
+            <form>
+                <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+                <div className='form-floating'>
+                    <input id='usernameInput' className='form-control' type='text' placeholder='Username'/>
+                    <label htmlFor='usernameInput'>Username</label>
+                </div>
+                <div className='form-floating'>
+                    <input id='passwordInput' className='form-control' type='password' placeholder='Password'/>
+                    <label htmlFor='passwordInput'>Password</label>
+                </div>
+                <button className='btn btn-primary w-100 py-2' type='submit'>Sign in</button>
+            </form>
+        </div>
     );
 };
