6f713c394d24663a20659755c027b0f1112eda7c
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-22 08:45:37 -0700

    added base login page

---
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Login/LoginPage.tsx
@@ -0,0 +1,16 @@
+export const LoginPage = () => {
+    return (
+        <form>
+            <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+            <div className='form-floating'>
+                <input id='usernameInput' className='form-control' type='username' placeholder='Username'/>
+                <label htmlFor='usernameInput'>Username</label>
+            </div>
+            <div className='form-floating'>
+                <input id='passwordInput' className='form-control' type='password' placeholder='Password'/>
+                <label htmlFor='passwordInput'>Password</label>
+            </div>
+            <button className='btn btn-primary w-100 py-2' type='submit'>Sign in</button>
+        </form>
+    );
+};
