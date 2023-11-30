55d5c22736a711ac1074510c2d88fcd6d316050d
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-30 12:04:32 -0700

    added unauthorized access block

--- Implementation Phase/01-frontend/react-flight/src/layouts/Admin/AdminPage.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Admin/AdminPage.tsx
@@ -1,11 +1,13 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {ViewFlights} from "./components/ViewFlights";
 import {ManageFlights} from "./components/ManageFlights";
 import {ManageCrew} from "./components/ManageCrew";
 import {ManageAircraft} from "./components/ManageAircraft";
 import {ManageUsers} from "./components/ManageUsers";
+import CurrentUserContext, {Roles} from "../../contexts/CurrentUserContext";
 
 export const AdminPage = () => {
+    const {currentUser, setCurrentUser} = useContext(CurrentUserContext);
 
     const [manageFlights, setManageFlights] = useState(false);
     const [manageCrew, setManageCrew] = useState(false);
@@ -49,60 +51,65 @@ export const AdminPage = () => {
 
     return (
         <div className='container'>
-            <div className='mt-5'>
-                <h3>Manage Data</h3>
-                <nav>
-                    <div className='nav nav-tabs' id='nav-tab' role='tablist'>
-                        <button onClick={viewFlightsClickFunction} className='nav-link active' id='nav-view-flights-tab'
-                                data-bs-toggle='tab' data-bs-target='#nav-view-flights' type='button'
-                                role='tab' aria-controls='nav-view-flights' aria-selected='true'>
-                            View flights
-                        </button>
-                        <button onClick={manageFlightClickFunction} className='nav-link ' id='nav-modify-flight-tab'
-                                data-bs-toggle='tab' data-bs-target='#nav-modify-flight' type='button'
-                                role='tab' aria-controls='nav-modify-flight' aria-selected='false'>
-                            Manage flights
-                        </button>
-                        <button onClick={manageCrewClickFunction} className='nav-link ' id='nav-modify-crew-tab'
-                                data-bs-toggle='tab' data-bs-target='#nav-modify-crew' type='button'
-                                role='tab' aria-controls='nav-modify-crew' aria-selected='false'>
-                            Manage crew
-                        </button>
-                        <button onClick={manageAircraftClickFunction} className='nav-link ' id='nav-modify-aircraft-tab'
-                                data-bs-toggle='tab' data-bs-target='#nav-modify-aircraft' type='button'
-                                role='tab' aria-controls='nav-modify-aircraft' aria-selected='false'>
-                            Manage aircraft
-                        </button>
-                        <button onClick={manageUsersClickFunction} className='nav-link ' id='nav-modify-users-tab'
-                                data-bs-toggle='tab' data-bs-target='#nav-modify-users' type='button'
-                                role='tab' aria-controls='nav-modify-users' aria-selected='false'>
-                            Manage users
-                        </button>
-                    </div>
-                </nav>
-                <div className='tab-content' id='nav-tabContent'>
-                    <div className='tab-pane fade show active' id='nav-view-flights' role='tabpanel'
-                         aria-labelledby='nav-view-flights-tab'>
-                        <ViewFlights/>
-                    </div>
-                    <div className='tab-pane fade' id='nav-modify-flight' role='tabpanel'
-                         aria-labelledby='nav-modify-flight-tab'>
-                        {manageFlights ? <ManageFlights/> : <></>}
-                    </div>
-                    <div className='tab-pane fade' id='nav-modify-crew' role='tabpanel'
-                         aria-labelledby='nav-modify-crew-tab'>
-                        {manageCrew ? <ManageCrew/> : <></>}
-                    </div>
-                    <div className='tab-pane fade' id='nav-modify-aircraft' role='tabpanel'
-                         aria-labelledby='nav-modify-aircraft-tab'>
-                        {manageAircraft ? <ManageAircraft/> : <></>}
-                    </div>
-                    <div className='tab-pane fade' id='nav-modify-users' role='tabpanel'
-                         aria-labelledby='nav-modify-aircraft-tab'>
-                        {manageUsers ? <ManageUsers/> : <></>}
+            {currentUser.role == 1 ?
+                <div className='mt-5'>
+                    <h3>Manage Data</h3>
+                    <nav>
+                        <div className='nav nav-tabs' id='nav-tab' role='tablist'>
+                            <button onClick={viewFlightsClickFunction} className='nav-link active' id='nav-view-flights-tab'
+                                    data-bs-toggle='tab' data-bs-target='#nav-view-flights' type='button'
+                                    role='tab' aria-controls='nav-view-flights' aria-selected='true'>
+                                View flights
+                            </button>
+                            <button onClick={manageFlightClickFunction} className='nav-link ' id='nav-modify-flight-tab'
+                                    data-bs-toggle='tab' data-bs-target='#nav-modify-flight' type='button'
+                                    role='tab' aria-controls='nav-modify-flight' aria-selected='false'>
+                                Manage flights
+                            </button>
+                            <button onClick={manageCrewClickFunction} className='nav-link ' id='nav-modify-crew-tab'
+                                    data-bs-toggle='tab' data-bs-target='#nav-modify-crew' type='button'
+                                    role='tab' aria-controls='nav-modify-crew' aria-selected='false'>
+                                Manage crew
+                            </button>
+                            <button onClick={manageAircraftClickFunction} className='nav-link ' id='nav-modify-aircraft-tab'
+                                    data-bs-toggle='tab' data-bs-target='#nav-modify-aircraft' type='button'
+                                    role='tab' aria-controls='nav-modify-aircraft' aria-selected='false'>
+                                Manage aircraft
+                            </button>
+                            <button onClick={manageUsersClickFunction} className='nav-link ' id='nav-modify-users-tab'
+                                    data-bs-toggle='tab' data-bs-target='#nav-modify-users' type='button'
+                                    role='tab' aria-controls='nav-modify-users' aria-selected='false'>
+                                Manage users
+                            </button>
+                        </div>
+                    </nav>
+                    <div className='tab-content' id='nav-tabContent'>
+                        <div className='tab-pane fade show active' id='nav-view-flights' role='tabpanel'
+                             aria-labelledby='nav-view-flights-tab'>
+                            <ViewFlights/>
+                        </div>
+                        <div className='tab-pane fade' id='nav-modify-flight' role='tabpanel'
+                             aria-labelledby='nav-modify-flight-tab'>
+                            {manageFlights ? <ManageFlights/> : <></>}
+                        </div>
+                        <div className='tab-pane fade' id='nav-modify-crew' role='tabpanel'
+                             aria-labelledby='nav-modify-crew-tab'>
+                            {manageCrew ? <ManageCrew/> : <></>}
+                        </div>
+                        <div className='tab-pane fade' id='nav-modify-aircraft' role='tabpanel'
+                             aria-labelledby='nav-modify-aircraft-tab'>
+                            {manageAircraft ? <ManageAircraft/> : <></>}
+                        </div>
+                        <div className='tab-pane fade' id='nav-modify-users' role='tabpanel'
+                             aria-labelledby='nav-modify-aircraft-tab'>
+                            {manageUsers ? <ManageUsers/> : <></>}
+                        </div>
                     </div>
                 </div>
-            </div>
+            :
+                <h3>Admin access only</h3>
+            }
+
         </div>
     );
 };
