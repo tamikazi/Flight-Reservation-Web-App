f47c0b0c235fd4f180cb4e84cb87d1ccdf65a28a
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-30 20:13:46 -0700

    added card details

--- Implementation Phase/01-frontend/react-flight/src/layouts/PaymentHistory/components/Receipt.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/PaymentHistory/components/Receipt.tsx
@@ -1,7 +1,11 @@
 import PaymentModel from "../../../models/PaymentModel";
+import React from "react";
 
 export const Receipt:React.FC<{ payment: PaymentModel }> = (props) => {
     return(
-        <div></div>
+        <div className='list-group-item d-flex gap-5 py-3'>
+            <h5>Date: {props.payment.payDate}</h5>
+            <h5>Amount: ${props.payment.amount}</h5>
+        </div>
     )
 }
