c15d7b612ba970a5315598f1eba8d2d678354c6e
Ian Saari <ian.saari1@ucalgary.ca>
2023-11-30 17:41:34 -0700

    added id to payment

--- Implementation Phase/01-frontend/react-flight/src/models/PaymentModel.tsx
+++ Implementation Phase/01-frontend/react-flight/src/models/PaymentModel.tsx
@@ -1,9 +1,11 @@
 class PaymentModel {
+    paymentId: number;
     userId: number;
     date: string;
     amount: number;
 
-    constructor(userId: number, date: string, amount: number) {
+    constructor(paymentId: number, userId: number, date: string, amount: number) {
+        this.paymentId = paymentId;
         this.userId = userId;
         this.date = date;
         this.amount = amount;
