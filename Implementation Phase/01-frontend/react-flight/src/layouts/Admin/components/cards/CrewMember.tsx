c1c47878744dc24c2f2c933bd4d8cab3863f91f8
Ian Saari <ian.saari1@ucalgary.ca>
2023-12-02 10:14:50 -0700

    added onclick function

--- Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/cards/CrewMember.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/cards/CrewMember.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CrewView from "../../../../models/CrewView";
 
-export const CrewMember:React.FC<{crewMember: CrewView}> = (props) => {
+export const CrewMember:React.FC<{
+    crewMember: CrewView,
+    onClick: any
+}> = (props) => {
     return (
-        <button className='list-group-item list-group-item-action d-flex gap-5 py-3'>
+        <button className='list-group-item list-group-item-action d-flex gap-5 py-3' onClick={props.onClick}>
             <h5>Name: {props.crewMember.name}</h5>
         </button>
     );
