28b7babf6a3a2265ed0ebe62e5c28d5f5dfb5b3c
Ian Saari <ian.saari1@ucalgary.ca>
2023-12-01 20:15:20 -0700

    updated variables to lowercase

--- Implementation Phase/01-frontend/react-flight/src/models/RegisterView.tsx
+++ Implementation Phase/01-frontend/react-flight/src/models/RegisterView.tsx
@@ -1,16 +1,16 @@
 class RegisterView {
     username: string;
     password: string;
-    Fname: string;
-    Lname: string;
+    fname: string;
+    lname: string;
     address: string;
     card: boolean;
 
     constructor(username: string, password: string, Fname: string, Lname: string, address: string, card: boolean) {
         this.username = username;
         this.password = password;
-        this.Fname = Fname;
-        this.Lname = Lname;
+        this.fname = Fname;
+        this.lname = Lname;
         this.address = address;
         this.card = card;
     }
