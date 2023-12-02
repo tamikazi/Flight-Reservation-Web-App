206169d6faa5efa4c199bb6bf835bc938d3f1b16
Ian Saari <ian.saari1@ucalgary.ca>
2023-12-01 22:18:50 -0700

    cleaned up code

--- Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/ViewFlights.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/ViewFlights.tsx
@@ -8,88 +8,40 @@ export const ViewFlights = () => {
     const [date, setDate] = useState('');
     const [flights, setFlights] = useState<FlightView[]>([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [httpError, setHttpError] = useState(null);
 
-    // useEffect(() => {
-        const fetchFlights = async () => {
-            let url: string = `http://localhost:8080/api/admin/flights/date/${date}`;
+    const fetchFlights = async () => {
+        let url: string = `http://localhost:8080/api/admin/flights/date/${date}`;
 
-            const response = await fetch(url);
+        const response = await fetch(url);
 
-            if (!response.ok) {
-                throw new Error('Something went wrong!');
-            }
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
 
-            const responseData = await response.json();
+        const responseData = await response.json();
 
-            const loadedFlights: FlightView[] = [];
+        const loadedFlights: FlightView[] = [];
 
-            for (const key in responseData) {
-                loadedFlights.push({
-                    flightId: responseData[key].flightID,
-                    code: responseData[key].code,
-                    origin: responseData[key].origin,
-                    destination: responseData[key].destination,
-                    date: responseData[key].date,
-                    time: responseData[key].time,
-                    aircraft: responseData[key].aircraftID,
-                    price: Number(responseData[key].basePrice)
-                });
-            }
-
-            // Fake data
-            const mockFlights: FlightView[] = [];
-            mockFlights.push({
-                flightId: '1',
-                code: 'AB100',
-                origin: 'Calgary',
-                destination: 'Vancouver',
-                date: '2023-11-30',
-                time: '0800',
-                aircraft: '1',
-                price: 100
+        for (const key in responseData) {
+            loadedFlights.push({
+                flightId: responseData[key].flightID,
+                code: responseData[key].code,
+                origin: responseData[key].origin,
+                destination: responseData[key].destination,
+                date: responseData[key].date,
+                time: responseData[key].time,
+                aircraft: responseData[key].aircraftID,
+                price: Number(responseData[key].basePrice)
             });
-            mockFlights.push({
-                flightId: '2',
-                code: 'AB101',
-                origin: 'Calgary',
-                destination: 'Edmonton',
-                date: '2023-11-30',
-                time: '1350',
-                aircraft: '2',
-                price: 100
-            });
-            mockFlights.push({
-                flightId: '3',
-                code: 'AB102',
-                origin: 'Vancouver',
-                destination: 'Calgary',
-                date: '2023-11-30',
-                time: '1530',
-                aircraft: '1',
-                price: 100
-            });
+        }
 
-            setFlights(loadedFlights);
-            setIsLoading(false);
-        };
-    //     fetchFlights().catch((error: any) => {
-    //         setIsLoading(false);
-    //         setHttpError(error.message);
-    //     })
-    // }, [triggerSearch]);
+        setFlights(loadedFlights);
+        setIsLoading(false);
+    };
 
     if (isLoading) {
         return (
             <SpinnerLoading/>
-        )
-    }
-
-    if (httpError) {
-        return (
-            <div className='container m-5'>
-                <p>{httpError}...</p>
-            </div>
         )
     }
 
