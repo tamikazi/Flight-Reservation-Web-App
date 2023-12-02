4734706095f8ea352e6929cf0f33ed88024e92ce
Ian Saari <ian.saari1@ucalgary.ca>
2023-12-01 22:51:59 -0700

    added admin functionality to manage aircraft page

--- Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/ManageAircraft.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/ManageAircraft.tsx
@@ -1,52 +1,47 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {SpinnerLoading} from "../../Utils/SpinnerLoading";
 import AircraftView from "../../../models/AircraftView";
 import {Aircraft} from "./cards/Aircraft";
+import AircraftRequestView from "../../../models/AircraftRequestView";
 
 export const ManageAircraft = () => {
 
+    const [model, setModel] = useState('');
+    const [rows, setRows] = useState(0);
+    const [columns, setColumns] = useState(0);
+
     const [aircraft, setAircraft] = useState<AircraftView[]>([]);
+    const [selectedAircraft, setSelectedAircraft] = useState<AircraftView|null>(null);
+    const [aircraftUpdated, setAircraftUpdated] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
-    const [searchUrl, setSearchUrl] = useState('');
+
+    const [displayWarning, setDisplayWarning] = useState(false);
+    const [failureWarning, setFailureWarning] = useState(false);
+    const [displaySuccess, setDisplaySuccess] = useState(false);
 
     useEffect(() => {
         const fetchAircraft = async () => {
-            // const baseUrl: string = "http://localhost:8080/api/flights";
-            //
-            // let url: string;
-            //
-            // if (searchUrl === '') {
-            //     url = `${baseUrl}`;
-            // } else {
-            //     url = `${baseUrl}${searchUrl}`;
-            // }
-            //
-            // const response = await fetch(url);
-            //
-            // if (!response.ok) {
-            //     throw new Error('Something went wrong!');
-            // }
-            //
-            // const responseData = await response.json();
-            //
-            // const loadedFlights: FlightView[] = [];
-            //
-            // for (const key in responseData) {
-            //     loadedFlights.push({
-            //         flightId: responseData[key].flightId,
-            //         code: responseData[key].code,
-            //         origin: responseData[key].origin,
-            //         destination: responseData[key].destination,
-            //         date: responseData[key].date,
-            //         aircraft: responseData[key].aircraft
-            //     });
-            // }
+            const url: string = "http://localhost:8080/api/admin/aircrafts/all";
 
-            // Generate random names
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseData = await response.json();
+
             const loadedAircraft: AircraftView[] = [];
-            loadedAircraft.push({aircraftID: 0, model: 'B787', numCols: 4, numRows: 8});
-            loadedAircraft.push({aircraftID: 1, model: 'A220', numCols: 6, numRows: 20});
+
+            for (const key in responseData) {
+                loadedAircraft.push({
+                    aircraftID: responseData[key].aircraftID,
+                    model: responseData[key].model,
+                    numCols: responseData[key].numCols,
+                    numRows: responseData[key].numRows
+                });
+            }
 
             setAircraft(loadedAircraft);
             setIsLoading(false);
@@ -55,7 +50,69 @@ export const ManageAircraft = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, [searchUrl]);
+    }, [aircraftUpdated]);
+
+    async function addAircraft() {
+        try {
+            const url = `http://localhost:8080/api/admin/aircrafts/add`;
+
+            const userRequest = new AircraftRequestView(model, columns, rows);
+
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userRequest)
+            };
+
+            const updateResponse = await fetch(url, requestOptions);
+
+            if(!updateResponse.ok) {
+                setFailureWarning(true);
+                return;
+            } else {
+                setFailureWarning(false);
+                setDisplaySuccess(true);
+                setAircraftUpdated(!aircraftUpdated);
+            }
+        } catch (error) {
+            setFailureWarning(true);
+        }
+    }
+
+    async function deleteAircraft() {
+        if(selectedAircraft) {
+            const url = `http://localhost:8080/api/admin/aircrafts/delete/${selectedAircraft?.aircraftID}`;
+            const requestOptions = {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            const updateResponse = await fetch(url, requestOptions);
+            if (!updateResponse.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            setSelectedAircraft(null);
+
+            // Trigger fetching all bookings
+            setAircraftUpdated(!aircraftUpdated);
+        }
+    }
+
+    const addHandle = () => {
+        setDisplayWarning(false);
+        setFailureWarning(false);
+        setDisplaySuccess(false);
+        if(model !== '' && rows !== 0 && columns !== 0) {
+            addAircraft();
+        } else {
+            setDisplayWarning(true);
+        }
+    }
 
     if (isLoading) {
         return (
@@ -81,22 +138,40 @@ export const ManageAircraft = () => {
                     <form>
                         <div className='row g-3 mb-5'>
                             <div className='col-4'>
-                                <label htmlFor='code' className='form-label'>Aircraft Model</label>
-                                <input type='text' className='form-control' id='model'/>
+                                <label htmlFor='model' className='form-label'>Aircraft Model</label>
+                                <input type='text' className='form-control' id='model'
+                                       onChange={e => setModel(e.target.value)}/>
                             </div>
                             <div className='col-4'>
-                                <label htmlFor='date' className='form-label'>Rows of Seats</label>
-                                <input type='number' className='form-control' id='rows'/>
+                                <label htmlFor='rows' className='form-label'>Rows of Seats</label>
+                                <input type='number' className='form-control' id='rows'
+                                       onChange={e => setRows(Number(e.target.value))}/>
                             </div>
                             <div className='col-4'>
-                                <label htmlFor='origin' className='form-label'>Columns of Seats</label>
-                                <input type='number' className='form-control' id='columns'/>
+                                <label htmlFor='columns' className='form-label'>Columns of Seats</label>
+                                <input type='number' className='form-control' id='columns'
+                                       onChange={e => setColumns(Number(e.target.value))}/>
                             </div>
-                            <div className='col'>
-                                <button className='btn btn-primary' type='button'>
+                            <div className='col-6'>
+                                <button className='btn btn-primary' type='button' onClick={addHandle}>
                                     Add
                                 </button>
                             </div>
+                            {displayWarning &&
+                                <div className='col-6 alert alert-danger' role='alert'>
+                                    All fields must be filled in
+                                </div>
+                            }
+                            {failureWarning &&
+                                <div className='col-6 alert alert-danger' role='alert'>
+                                    Failed to add aircraft
+                                </div>
+                            }
+                            {displaySuccess &&
+                                <div className='col-6 alert alert-success' role='alert'>
+                                    Registration successful
+                                </div>
+                            }
                         </div>
                     </form>
                     <div>
@@ -104,7 +179,8 @@ export const ManageAircraft = () => {
                             <>
                                 <div className='list-group'>
                                     {aircraft.map((aircraft, index) => (
-                                        <Aircraft aircraft={aircraft} key={index}/>
+                                        <Aircraft aircraft={aircraft} key={index}
+                                                  select={() => setSelectedAircraft(aircraft)}/>
                                     ))}
                                 </div>
                             </>
@@ -115,7 +191,7 @@ export const ManageAircraft = () => {
                                 </div>
                             </>
                         }
-                        <button className='btn btn-primary mt-3' onClick={() => {}}>
+                        <button className='btn btn-primary mt-3' onClick={deleteAircraft}>
                             Delete
                         </button>
                     </div>
--- Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/cards/Aircraft.tsx
+++ Implementation Phase/01-frontend/react-flight/src/layouts/Admin/components/cards/Aircraft.tsx
@@ -1,12 +1,18 @@
 import AircraftView from "../../../../models/AircraftView";
 import React from "react";
 
-export const Aircraft: React.FC<{ aircraft: AircraftView }> = (props) => {
+export const Aircraft: React.FC<{
+    aircraft: AircraftView,
+    select: any
+}> = (props) => {
     return (
-        <button className='list-group-item list-group-item-action d-flex gap-5 py-3'>
+        <button className='list-group-item list-group-item-action d-flex gap-5 py-3' onClick={props.select}>
             <h4>{props.aircraft.model}</h4>
-            <div className='d-flex gap-2 w-100 justify-content-between'>
-                <div>
+            <div className='d-flex flex-row'>
+                <div className='mx-3'>
+                    <h6 className='mb-0'>ID number: {props.aircraft.aircraftID}</h6>
+                </div>
+                <div className='mx-3'>
                     <h6 className='mb-0'>Rows of seats: {props.aircraft.numRows}</h6>
                     <h6 className='mb-0'>Columns of seats: {props.aircraft.numCols}</h6>
                 </div>
