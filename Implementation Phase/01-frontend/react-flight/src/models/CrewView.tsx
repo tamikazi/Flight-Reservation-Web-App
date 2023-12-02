54e72589c3cbb1869df481c9e8f25d4c7a7c6632
Ian Saari <ian.saari1@ucalgary.ca>
2023-12-02 10:14:39 -0700

    added userID and flightID

--- Implementation Phase/01-frontend/react-flight/src/models/CrewView.tsx
+++ Implementation Phase/01-frontend/react-flight/src/models/CrewView.tsx
@@ -1,8 +1,11 @@
 class CrewView {
+    userID: number;
+    flightID: number;
     name: string;
 
-
-    constructor(name: string) {
+    constructor(userID: number, flightID: number, name: string) {
+        this.userID = userID;
+        this.flightID = flightID;
         this.name = name;
     }
 }
