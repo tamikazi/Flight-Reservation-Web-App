e9581209fd051f56e2512b7d981858b7c0c0b173
tamikazi <tahmidkazi829@gmail.com>
2023-12-02 17:02:18 -0700

    added cross origin to newsletter

--- Implementation Phase/02-backend/spring-flight/src/main/java/com/ensf614/springflight/controller/NewsletterController.java
+++ Implementation Phase/02-backend/spring-flight/src/main/java/com/ensf614/springflight/controller/NewsletterController.java
@@ -3,11 +3,13 @@ package com.ensf614.springflight.controller;
 import com.ensf614.springflight.newsletter.NewsletterSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ensf614.springflight.newsletter.NewsletterTemplate;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/newsletter")
 public class NewsletterController {
